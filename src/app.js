4cbed4d20a197cd139b48125cc5631c44792d33d
Ivanjaviersanchez <ivanjaviersanchez0108@gmail.com>
2024-01-27 00:33:06 +0100

    Entrega final 27/01

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@
              //-----FUNCIONES GLOBALES------
 
     //Agregar productos al stock
-    let productoID = 1;        
+    let productoID = productos.length;        
     class Producto {
         constructor(nombre, stock, precio){
             this.nombre = nombre;
@@ -24,27 +24,35 @@
     }      
 
     const cargarProducto = (producto) => {
-        productos.push(producto);
-        alert(`Ingreso el producto: ${producto.nombre} con el ID: ${productoID}`);
         productoID++;
+        producto.nombre = producto.nombre.toLowerCase();
+        productos.push(producto);
+        alert(`Se ingreso el producto: ${producto.nombre} con el ID: ${productoID}`);
+        
         localStorage.setItem("productos", JSON.stringify(productos));
         console.log(productos);
     }
 
     const ingresarProducto = () => {
         let nombreInput = document.querySelector("#nombreInput");
-        let stockInput = document.querySelector("#stockInput");
-        let precioInput = document.querySelector("#precioInput");
-      
-        let producto = new Producto(nombreInput.value, stockInput.value, precioInput.value);
+        let stockInput = parseInt(document.querySelector("#stockInput").value);
+        let precioInput = parseFloat(document.querySelector("#precioInput").value);
+       
+        let producto = new Producto(nombreInput.value, stockInput, precioInput);
         
-        cargarProducto(producto);  
+        if(productos.some(producto => producto.nombre == nombreInput.value.toLowerCase())){
+            alert("El producto ingresado ya existe");
+        }else{   
+            cargarProducto(producto); 
+        }
     };
-    
+
+
     const eliminarNombre = (nombreEliminarProducto) => {
-        let existe = productos.some(producto => producto.nombre === nombreEliminarProducto);
+        let nombreEliminarProductoMYM = nombreEliminarProducto.toLowerCase();
+        let existe = productos.some(producto => producto.nombre === nombreEliminarProductoMYM); 
         if(existe){
-            productos = productos.filter(producto => producto.nombre !== nombreEliminarProducto);       
+            productos = productos.filter(producto => producto.nombre !== nombreEliminarProductoMYM);       
             alert(`Se elimino el producto: ${nombreEliminarProducto}`);
             localStorage.setItem("productos", JSON.stringify(productos));
             }else{
@@ -58,14 +66,39 @@
         return nombreEliminarProducto ;
             
     };
-    
+     
 
-    const agregarStock=()=>{
+    const agregarStock = () => {
+        let nombreAgregarInput = document.querySelector("#nombreAgregarInput").value.toLowerCase();
+        let unidadesInput = parseInt(document.querySelector("#unidadesInput").value);
     
-    }
-    const modificarPrecio=()=>{
+        let productoEncontrado = productos.find(producto => producto.nombre === nombreAgregarInput);
+        
+        if (productoEncontrado) {
+           /*  parseInt(productoEncontrado.stock).value; */
+            productoEncontrado.stock += unidadesInput;
+            alert(`Se agregaron ${unidadesInput} unidades al stock de ${nombreAgregarInput}. Stock actual: ${productoEncontrado.stock}`);
+            localStorage.setItem("productos", JSON.stringify(productos));
+        } else {
+            alert("El nombre ingresado no existe");
+        }  
+    };
+
+   
+    const modificarPrecio = () => {
+        let nombreModificarInput = document.querySelector("#precioModificarinput").value.toLowerCase();
+        let precioActualizadoInput = parseInt(document.querySelector("#precioActualizadoinput").value);
     
-    }
+        let productoEncontrado = productos.find(producto => producto.nombre === nombreModificarInput);
+    
+        if (productoEncontrado) {
+            productoEncontrado.precio = precioActualizadoInput;
+            alert(`Se ha actualizado el precio de ${nombreModificarInput} a $ ${precioActualizadoInput}`);
+            localStorage.setItem("productos", JSON.stringify(productos));
+        } else {
+            alert("El nombre ingresado no existe");
+        }
+    };
 
                // ------EJECUTANDO APLICACION------
     console.log("Ejecutando aplicación");
@@ -100,19 +133,26 @@
         console.log(productos);
         formModificarPrecio.reset();
     };
-
-      // PLANTILLA DE LISTA DE PRODUCTOS 
     
-    productos.forEach ( producto=>{
-            let tarjetaProducto = document.createElement("id-listaProductos");
+    document.getElementById("btnActualizarLista").addEventListener("click", () => {
+        // Limpia la lista actual
+        listaProductos.innerHTML = "";
+
+        // PLANTILLA DE LISTA DE PRODUCTOS 
+        productos.forEach ( producto=>{
+            let tarjetaProducto = document.createElement("listaProductos");
             tarjetaProducto.className = "border border-2 p-3 w-50 m-3";
             tarjetaProducto.innerHTML = `<p> id: ${producto.productoID}</p>
                                          <p> nombre: ${producto.nombre}</p>
                                          <p> stock: ${producto.stock}</p>
                                          <p> precio: ${producto.precio}</p>;
-                                         `;
+                                     `;
             listaProductos.appendChild(tarjetaProducto);                             
-    }); 
+        }); 
+    });
+
+
+
     
 
 
